b0194e7b446eb62fe4416d35dd862a2c3dcdf161
Scott <scottcseagraves@gmail.com>
2016-08-29 03:07:31 +0000

    ASCII Art

---
+++ lab1/lab1.cpp
@@ -0,0 +1,29 @@
+//Scott Seagraves
+//8/24/2016
+//ASCII ART
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+std:: 
+cout<<"#####################################################################"<<endl;
+	cout<<"#                                                               #"<<endl;  
+	cout<<"#                          ___________                          #"<<endl;  
+	cout<<"#                               |                               #"<<endl;  
+	cout<<"#                               |                               #"<<endl;  
+	cout<<"#                         _     |     _                         #"<<endl;  
+	cout<<"#                        / \\   _|_   / \\                        #"<<endl;  
+	cout<<"#                       ( O )-/   \\-( O )                       #"<<endl;  
+	cout<<"#  _                     \\_/ /\\___/\\ \\_/                     _  #"<<endl;  
+	cout<<"# (_)_______________________( ( . ) )_______________________(_) #"<<endl;  
+	cout<<"#                                                               #"<<endl;  
+	cout<<"#                                                               #"<<endl;  
+	cout<<"#                                             _                 #"<<endl;  
+	cout<<"#                                                               #"<<endl;  
+	cout<<"#                                                               #"<<endl;  
+	cout<<"#                                                               #"<<endl;  
+	cout<<"#                                                               #"<<endl;  
+	cout<<"#################################################################"<<endl;
+           }
