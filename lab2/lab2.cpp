1a0f05aad7bbbd4bdf0acf73661a16718348aadb
Scott <scottcseagraves@gmail.com>
2016-08-29 03:11:03 +0000

    Lab 2 Submission

---
+++ lab2/lab2.cpp
@@ -0,0 +1,18 @@
+//Scott Seagraves
+//8/28/2016
+//Simple Number guessing game
+
+//Import library
+//Create workspace
+//Create main
+//Create thenumber equals to generate randomn number
+//Create scorecard=10
+//Create loop start for incorrect guesses.
+//Create user entry of (guess) 
+//Create If then statement. Something if guess = thenumber then cout<<"You guessed correctly! Your score is + (scorecard) Play again?"<<endl ;
+//Create loop activation value y=true n=false.
+//      Then guess < thenumber scorecard - 1 cout<<"Sorry. Your number was too low. Your score is now + scorecard Try again? y= yes n= no"<<end1;
+//Create loop activation value y=true n=false.
+//      Then guess > thenumber scorecard - 1 cout<<"Sorry. Your number was too high. Your score is now + scorecard Try again?  y= yes n= no"<<end1;
+//Create loop activation value y=true n=false 
+//n=quit main 
