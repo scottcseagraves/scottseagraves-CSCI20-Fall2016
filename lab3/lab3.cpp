d106eb2f894a04b617d52c9102dcb7bc24f6f560
Scott <scottcseagraves@gmail.com>
2016-09-07 06:21:32 +0000

    Lab 4 Submission

--- lab3/lab3.cpp
+++ lab3/lab3.cpp
@@ -18,6 +18,10 @@ std::
    string noun4 = "";
    string place = "";
    string adjective = "";
+   int num = 72;
+   double num1 = 3.4;
+   char name[] = "sunshine";
+   
    
 cout<<"Welcome to Scott's mad lib."<<endl;
 
@@ -40,7 +44,7 @@ cout<<"Please choose a another noun (last on I promise)."<<endl;
 cin>> noun4;
 
 cout<<"Be kind to your "<< noun1<<"-footed "<<noun2<< " For a duck may be somebody`s "<< noun3<<", Be kind to your "<<endl;
-cout<<noun2<< " in "<< place<< " Where the weather is always "<<adjective <<". You may think that this is the "<< noun4 << ", Well it is."<<endl;
+cout<<noun2<< " in "<< place<< ". Where the weather is always "<<adjective <<". You may think that this is the "<< noun4 << ", Well it is."<< "Your lucky numbers are " << num << " and " << num1 << " and your lucky word is "<< name << endl;
 
 return 0;
 }
---
+++ lab4/lab4.cpp
@@ -0,0 +1,42 @@
+//Scott Seagraves
+//9/2/2016
+//coin star
+
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+std::
+    
+    cout<<"Welcome to coin star, we're here to steal your money."<<endl;
+
+ int c = 0;
+    
+    int x = 25;
+    int y = 10;
+    int z = 5;
+
+    
+cout<<"Please insert the amount of change you have in cents"<<endl;
+cin>> c;
+
+
+
+    cout<< "You have " << c / x << " quarters" <<endl;
+    cout<< "and " << (c % x) / y << " dimes" <<endl;
+    cout<< "and " << ((c % x) % y) / z << " nickles" <<endl;
+    
+    double a = (c / 100);  //amount total in dollars
+    
+    cout<< "Your total was $" << a  <<endl;
+    
+    double b =(a-(a * .109));  //amount after charged fee
+    
+    cout<< "After our fee of 10.9% you have $" << b <<endl;
+    
+    return 0;
+    
+}
